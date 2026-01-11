7f525c5337cbb397b5fdb3b0de41defa4032abff
AryanAAB <aryanbansal310@gmail.com>
2026-01-11 12:24:54 +0530

    Set Up Spring Boot

---
+++ src/test/java/org/example/microgrid/MicroGridApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.microgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGridApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
