253473564d9d9c5f5a7eaf23497e32ce201738fb
AryanAAB <aryanbansal310@gmail.com>
2026-01-25 17:01:29 +0530

    Added Bill Class

--- src/main/java/org/example/microgrid/house/Bill.java
+++ src/main/java/org/example/microgrid/house/Bill.java
@@ -2,9 +2,26 @@ package org.example.microgrid.house;
 
 public class Bill
 {
-    private double exportEnergyToGrid = 0;
-    private double importEnergyFromGrid = 0;
+    private double netEnergyFromGrid = 0;
     private double earnings = 0;
 
+    public void addEnergy(double energy)
+    {
+        netEnergyFromGrid += energy;
+    }
 
+    public void addEarnings(double earnings)
+    {
+        this.earnings += earnings;
+    }
+
+    public double getNetEnergyFromGrid()
+    {
+        return netEnergyFromGrid;
+    }
+
+    public double getEarnings()
+    {
+        return earnings;
+    }
 }
