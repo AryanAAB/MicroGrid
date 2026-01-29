e8493dacfba1d10bd55df998b64bb852d53ea27c
AbhirathA <153723704+AbhirathA@users.noreply.github.com>
2026-01-29 22:53:53 +0530

    Update LANManager.java

--- src/main/java/org/example/microgrid/lan/LANManager.java
+++ src/main/java/org/example/microgrid/lan/LANManager.java
@@ -9,7 +9,8 @@ import java.util.stream.Collectors;
 public class LANManager
 {
 
-    private final Map<String, LAN> lans = new HashMap<>();
+    private final Map<String, LAN> lans = new HashMap<>(); // Lan Id -> LAN Object
+    private final Map<House, String> houseLanMap = new HashMap<>(); // House Object -> LAN id
 
     public LANManager()
     {
@@ -19,18 +20,20 @@ public class LANManager
     public void initializeMapping()
     {
         // Initialize Firebase users.
-        // Every user has a meterID.
+        // Every user has a meterID in firebase.
         // The first letter of the meter ID is the ID of the newly created lan.
-        // Constructor of House is     public House( String id <use same as meter id of this user in firebase>, double initialProduction <set 2.5>, double initialConsumption <set 0.5>, double sellThreshold <set 0.2>, double costPrice <use buyBidPrice of this user from firebase>, double sellingPrice <use sellingPrice from firebase>)
+        // Constructor of House is public House( String id <use same as meter id of this user in firebase>, double initialProduction <set 2.5>, double initialConsumption <set 0.5>, double sellThreshold <set 0.2>, double costPrice <use buyBidPrice of this user from firebase>, double sellingPrice <use sellingPrice from firebase>)
         // Constructor of Lan is just public LAN()
+        // You add a House to a lan using public void addHouse(House house) of the lan object.
     }
 
-    public void runMarketCycles()
+    public void runMarketCycles() // called every t2
     {
         lans.forEach((key, lan) -> lan.runMarketCycle());
     }
 
-    public void step(Instant timestamp, double fractionOfDay)
+    //called every t1
+    public void step(Instant timestamp, double fractionOfDay) //timestamp can be <currentTime.toInstant()> // use private static final double FRACTION_OF_DAY_PER_STEP = 1.0 / (24 * 60); and reset steps at the end of the day (24hrs worth of steps)
     {
         lans.forEach((key, lan) -> lan.step(timestamp, fractionOfDay));
     }
@@ -41,5 +44,26 @@ public class LANManager
                 .stream()
                 .flatMap(lan -> lan.getBills().stream())
                 .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public void addUser(){
+        //Add user when new registration happens. You can add parameters to it.
+    }
+
+    public void uploadToFirebase(){ //called at the end of t3
+        //Do this.getBills and get a List of bill objects.
+        //bill objects do not have users, maybe try to query it seperately per user from the lan using lan.getBill(houseID)
+
+        /*
+            The bill object has:
+    public double getGridImported() //use to fill the specific user's energyConsumed in firebase.
+
+    public double getGridExported() //use to fill the specific user's energySold in firebase.
+
+    public double getP2PRevenue() // use this to fill the specific user's gridSavings in firebase.
+
+    public double getNetBill() // use this to fill the specific user's earnings in firebase.
+        */
+        
     }
 }
